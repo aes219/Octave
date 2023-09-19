575a5651a3f6c895c102a361342a2dde2b8885c0
Vaidik Rungta <rungtavaidik@gmail.com>
2023-09-19 18:19:27 +0530

    [backend] Update app.js
    Add headers to the backend server

--- src/backend/app.js
+++ src/backend/app.js
@@ -8,6 +8,13 @@ const port = process.env.PORT || 8000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+app.use(allowCrossDomain);
 
 app.listen(port, () => {
   console.log(`${chalk.yellow('[ BACKEND ]')} ${chalk.green(chalk.magenta(`Server listening on port ${port}`))}`)

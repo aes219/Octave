e4c9031a8aa70bbb97f96178ddee9f723bf42184
Vaidik Rungta <rungtavaidik@gmail.com>
2023-09-25 18:23:37 +0530

    Update SignUp.jsx
    Fix minor bugs

--- src/frontend/src/components/SignUp.jsx
+++ src/frontend/src/components/SignUp.jsx
@@ -37,7 +37,7 @@ function SignUp() {
         setSignupcomponent('none')
         setOtpcomponent('block')
 
-        axios.post(`${api}/mail?recipient=rungtavaidik@gmail.com&subject=One+Time+Password&msg=${otpCode}`)
+        axios.post(`${api}/mail?recipient=${email}&subject=One+Time+Password&msg=${otpCode}`)
     }
 
     async function checkOTP() {
@@ -94,83 +94,79 @@ function SignUp() {
         }
     }
     return (
-        <div>
-            <div id="otpcomponent" style={{ display: `${otpcomponent}`, margin: 170 }}>
-                <h1 align="center" className='text-3xl m-10'>
-                    <strong>
-                        {otpLabel}
-                    </strong>
-                </h1>
-                <Hero>
-                    <Hero.Content className="flex-col lg:flex-row-reverse">
-                        <Card className="flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                            <Card.Body>
-                                <Form>
-                                    <Input
-                                        type='number'
-                                        value={otp}
-                                        onChange={e => setOtp(e.target.value)}
-                                    />
-                                </Form>
-                                <Button color='info' variant='outline' className='no-animation' onClick={checkOTP}>Submit</Button>
-                            </Card.Body>
-                        </Card>
-                    </Hero.Content>
-                </Hero>
-                <div className="font-sans">
-                </div>
-            </div>
-
-            <div id="sigupcomponent" style={{ display: `${signupcomponent}`, margin: 80 }}>
-                <h1 align="center" className='text-3xl m-10'>
-                    <strong>
-                        Welcome to Octave!
-                        <br />
-                        Please SignUp below:
-                    </strong>
-                </h1>
-                <Hero>
-                    <Hero.Content className="flex-col lg:flex-row-reverse">
-                        <Card className="flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                            <Card.Body>
-                                <Form>
-                                    <Form.Label title={emailLabel} />
-                                    <Input
-                                        type="email"
-                                        placeholder="hello@example.com"
-                                        className={`input-bordered ${emailExists ? 'input-error' : ''}`}
-                                        value={email}
-                                        onChange={e => setEmail(e.target.value)}
-                                        name='email'
-                                    />
-                                </Form>
-                                <Form>
-                                    <Form.Label title={passLabel} />
-                                    <Input
-                                        type="password"
-                                        placeholder="Type a strong password..."
-                                        className={`input-bordered ${passExists ? 'input-error' : ''}`}
-                                        value={pass}
-                                        onChange={e => setPass(e.target.value)}
-                                        name='password'
-                                    />
-                                    <label className='label'>
-                                        <Link href="/" className="label-text-alt" hover>
-                                            Already have an account? Login here →
-                                        </Link>
-                                    </label>
-                                </Form>
-                                <Form className="mt-6">
-                                    <Button color='info' variant='outline' className='no-animation' onClick={handleSubmit}>Sign Up</Button>
-                                </Form>
-                            </Card.Body>
-                        </Card>
-                    </Hero.Content>
-                </Hero>
-                <div className="font-sans">
-                </div>
-            </div>
+        <>
+                <div id="otpcomponent" style={{ display: otpcomponent, margin: 170 }}>
+            <h1 align="center" className='text-3xl m-10'>
+                <strong>
+                    {otpLabel}
+                </strong>
+            </h1>
+            <Hero>
+                <Hero.Content className="flex-col lg:flex-row-reverse">
+                    <Card className="flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <Card.Body>
+                            <Form>
+                                <Input
+                                    type='number'
+                                    value={otp}
+                                    onChange={e => setOtp(e.target.value)}
+                                />
+                            </Form>
+                            <Button color='info' variant='outline' className='no-animation' onClick={checkOTP}>Submit</Button>
+                        </Card.Body>
+                    </Card>
+                </Hero.Content>
+            </Hero>
         </div>
+        {/* Signup Component */}
+        <div id="sigupcomponent" style={{ display: signupcomponent, margin: 80 }}>
+            <h1 align="center" className='text-3xl m-10'>
+                <strong>
+                    Welcome to Octave!
+                    <br />
+                    Please SignUp below:
+                </strong>
+            </h1>
+            <Hero>
+                <Hero.Content className="flex-col lg:flex-row-reverse">
+                    <Card className="flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <Card.Body>
+                            <Form>
+                                <Form.Label title={emailLabel} />
+                                <Input
+                                    type="email"
+                                    placeholder="hello@example.com"
+                                    className={`input-bordered ${emailExists ? 'input-error' : ''}`}
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                    name='email'
+                                />
+                            </Form>
+                            <Form>
+                                <Form.Label title={passLabel} />
+                                <Input
+                                    type="password"
+                                    placeholder="Type a strong password..."
+                                    className={`input-bordered ${passExists ? 'input-error' : ''}`}
+                                    value={pass}
+                                    onChange={e => setPass(e.target.value)}
+                                    name='password'
+                                />
+                                <label className='label'>
+                                    <Link href="/" className="label-text-alt" hover>
+                                        Already have an account? Login here →
+                                    </Link>
+                                </label>
+                            </Form>
+                            <Form className="mt-6">
+                                <Button color='info' variant='outline' className='no-animation' onClick={handleSubmit}>Sign Up</Button>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                </Hero.Content>
+            </Hero>
+        </div>
+        </>
     )
 }
 

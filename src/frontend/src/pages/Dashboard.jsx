6e5d4438da59f56e24c8ca0720ec88bc8978f3be
Vaidik Rungta <rungtavaidik@gmail.com>
2023-09-27 18:10:56 +0530

    [frontend] Update Dashboard.jsx
    Add profile option, to set a nickname and bio for each profile.

--- src/frontend/src/pages/Dashboard.jsx
+++ src/frontend/src/pages/Dashboard.jsx
@@ -1,23 +1,92 @@
-import { Drawer, Menu } from "react-daisyui";
+import axios from 'axios'
+import { useEffect } from "react";
+import { useState } from "react";
+import { Hero, Input, Card, Button, Textarea, Form } from "react-daisyui";
+const api = `http://localhost:8000`;
 
 function Dashboard() {
-    return(
+    const [profilecomponent, setProfileComponent] = useState('')
+    const [dashcomponent, setDashComponent] = useState('')
+    const [name, setName] = useState('')
+    const [bio, setBio] = useState('')
+    const [nameExists, setNameExists] = useState(true)
+    const [bioExists, setBioExists] = useState(true)
+
+    useEffect(() => {
+        if (window.localStorage.getItem("e")) {
+            setDashComponent('none')
+            setProfileComponent('block')
+        }
+        else {
+            setDashComponent('block')
+            setProfileComponent('none')
+        }
+    })
+
+    const accountSetup = () => {
+        if (!name && !bio) {
+            setNameExists(false)
+            setBioExists(false)
+        } else if (!name) {
+            setNameExists(false)
+            setBioExists(true)
+        } else if (!bio) {
+            setBioExists(false)
+            setNameExists(true)
+        } else {
+            axios.post(`${api}/users/profiles?email=${window.localStorage.getItem('e')}&nick=${name}&bio=${bio}`)
+            .then(
+                window.localStorage.removeItem('e'),
+                setProfileComponent('none'),
+                setDashComponent('block')
+            )
+        }
+
+    }
+    return (
         <>
-        <Drawer>
-        <div className="flex w-full p-4 items-center">
-            <Menu>
-                <Menu.Item className="bordered">
-                    <a>Item 1</a>
-                </Menu.Item>
-                <Menu.Item className="hover-bordered">
-                    <a>Item 2</a>
-                </Menu.Item>
-                <Menu.Item className="hover-bordered">
-                    <a>Item 3</a>
-                </Menu.Item>
-            </Menu>
-        </div>
-        </Drawer>
+            <div id="profilecomponent" style={{ display: profilecomponent, margin: 80 }}>
+                <h1 align="center" className='text-3xl m-10'>
+                    <strong>
+                        Please finish your account setup
+                    </strong>
+                </h1>
+                <Hero>
+                    <Hero.Content className="flex-col lg:flex-row-reverse">
+                        <Card className="flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                            <Card.Body>
+                                <Form>
+                                    <Form.Label title="Nickname" />
+                                    <Input
+                                        type='text'
+                                        value={name}
+                                        onChange={e => setName(e.target.value)}
+                                        className={`input-bordered ${(nameExists) ? `` : `input-error`}`}
+                                    />
+                                    <Form.Label title="About Me" />
+                                    <Textarea
+                                        value={bio}
+                                        onChange={e => setBio(e.target.value)}
+                                        style={{
+                                            height: 150,
+                                            width: 300
+                                        }}
+                                        className={`input-bordered ${(bioExists) ? `` : `textarea-error`}`}
+                                    />
+                                </Form>
+                                <Button color='info' variant='outline' className='no-animation' onClick={accountSetup}>Done!</Button>
+                            </Card.Body>
+                        </Card>
+                    </Hero.Content>
+                </Hero>
+            </div>
+            <div style={{display: dashcomponent}}>
+                <Hero>
+                    <Hero.Content>
+                        <h1 className="text-9xl font-bold">Dashboard</h1>
+                    </Hero.Content>
+                </Hero>
+            </div>
         </>
     )
 }

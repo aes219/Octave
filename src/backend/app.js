0441c95f1bf2607f58c36a639c316c23b90fb60e
Vaidik <rungtavaidik@gmail.com>
2023-11-20 17:47:32 +0530

    [backend] Use body-parser

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,19 +2,24 @@ require("dotenv").config();
 
 const express = require('express')
 const chalk = require('chalk')
+const bodyparser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 8000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
 var allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 }
-app.use(allowCrossDomain);
+
+app.use(allowCrossDomain)
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`${chalk.yellow('[ BACKEND ]')} ${chalk.green(chalk.magenta(`Server listening on port ${port}`))}`)

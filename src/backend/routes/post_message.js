51821eea411157efedc80a50ad14f36dabe991b0
Vaidik <rungtavaidik@gmail.com>
2024-01-08 18:34:11 +0530

    [backend] New POST /users/messages

---
+++ src/backend/routes/post_message.js
@@ -0,0 +1,43 @@
+require("dotenv").config()
+const { google } = require("googleapis")
+const sheets = google.sheets({ version: "v4" })
+const creds = './credentials.json'
+
+module.exports = {
+    route: "users/messages",
+    method: 'POST',
+    run: async (req, res) => {
+        try {
+            const { email, type, message } = req.query
+            const auth = await google.auth.getClient({
+                keyFile: creds,
+                scopes: ['https://www.googleapis.com/auth/spreadsheets']
+            });
+
+            const resource = {
+                values: [[email, type, message]],
+            };
+            const spreadsheetId = process.env.DATABASE_ID
+            const range = 'Messages!A:C'
+
+            sheets.spreadsheets.values.append({
+                auth,
+                spreadsheetId,
+                range,
+                valueInputOption: 'USER_ENTERED',
+                resource,
+            })
+
+            res.status(200).json({
+                status: 200,
+                message: "OK",
+            })
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({
+                status: 500,
+                message: "INTERNAL SERVER ERROR"
+            })
+        }
+    }
+}

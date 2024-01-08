57e9dafb1a74640706a8d101daf49d63b3991cb9
Vaidik <rungtavaidik@gmail.com>
2024-01-08 18:21:44 +0530

    [frontend] Update ChatWindow.jsx
    Fix duplicate marginLeft warning

--- src/frontend/src/components/ChatWindow.jsx
+++ src/frontend/src/components/ChatWindow.jsx
@@ -28,7 +28,7 @@ function ChatWindow() {
             <div>
                 <div style={{ position: 'fixed', bottom: 5, left: 0, right: 0 }}>
                     <Join>
-                        <Input style={{ width: 1000, marginLeft: 250, marginLeft: 260 }} placeholder='Send a message' className='join-item' />
+                        <Input style={{ width: 1000, marginLeft: 260 }} placeholder='Send a message' className='join-item' />
                         <Button className='join-item rounded-s' color='success'>
                             <svg className='h-10' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" style={{ strokeWidth: 2, strokeLinecap: 'square', strokeLinejoin: 'round' }}>
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />

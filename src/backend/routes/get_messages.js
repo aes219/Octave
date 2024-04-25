4ecb2fc46cbd2f5009385826e7f0a46939be9fd2
Vaidik <rungtavaidik@gmail.com>
2024-04-25 23:12:06 +0530

    Update get_message.js
    - Bit of performance improvement

--- src/backend/routes/get_messages.js
+++ src/backend/routes/get_messages.js
@@ -1,31 +1,36 @@
 const axios = require('axios')
+const { google } = require("googleapis")
+const sheets = google.sheets({ version: "v4" })
+const creds = './credentials.json'
 
 module.exports = {
     route: "users/messages",
     method: 'GET',
-    run: async(req, res) => {
-        const { client, recipient } = req.query
-        axios.get(require('../api')('Messages'))
-            .then((response) => {
-                const [headerRow, ...rows] = response.data.values;
+    run: async (req, res) => {
+        try {
+            const { client, recipient } = req.query;
+            const response = await axios.get(require('../api')('Messages'));
+            const { values } = response.data;
+            const headerRow = values.shift();
 
-                const Data = rows.map(row => {
+            const Data = values
+                .filter(row =>
+                    (row[0] == client && row[1] == recipient)
+                    ||
+                    (row[0] == recipient && row[1] == client)
+                )
+                .map(row => {
                     const obj = {};
                     headerRow.forEach((key, index) => {
-                        obj[key] = row[index]
-                    })
-                    
-                    return obj
-                })
+                        obj[key] = row[index];
+                    });
+                    return obj;
+                });
 
-                const result = Data.filter(entry =>
-                (entry.client == client && entry.recipient == recipient)
-                    ||
-                (entry.client == recipient && entry.recipient == client)
-                )
-
-                // console.log(result)
-                res.send(result)
-            })
+            res.send(Data);
+        } catch (error) {
+            console.error("Error fetching and filtering messages:", error);
+            res.status(500).send("Internal Server Error");
+        }
     }
-}
+};

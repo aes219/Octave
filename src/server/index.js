6f6b1fcd18f6d9d3cc5170b55082da6789ea7235
aes219 <rungtavaidik@gmail.com>
2024-10-17 09:12:09 +0530

    Fix cors origin issue and server constant

--- src/server/index.js
+++ src/server/index.js
@@ -3,21 +3,23 @@ require("dotenv").config();
 const express = require('express');
 const chalk = require('chalk');
 const bodyParser = require('body-parser');
+const http = require("http")
 const app = express();
 const port = process.env.PORT || 8000;
-const origin = "https://octave-chat.vercel.app"
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 var allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Credentials', 'true'); 
   next();
 };
+
+const server = http.createServer(app);
 
 app.use(allowCrossDomain);
 app.use(bodyParser.json());

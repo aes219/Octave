c649ad7a91d123d91242236ea60dedf84b26dc16
Vaidik Rungta <rungtavaidik@gmail.com>
2025-04-01 22:54:33 +0530

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 });
 
 var allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', process.env.CLIENT || 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Credentials', 'true'); 
